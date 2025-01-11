57c968867cd8b4be33f4f095a90f608eacd200f7
RanjanaRaghavan <52031047+RanjanaRaghavan@users.noreply.github.com>
2025-01-11 15:28:44 -0800

    Time: 34 ms (24.00%) | Memory: 53.6 MB (36.00%) - LeetSync

---
+++ 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {boolean}
+ */
+var canConstruct = function(s, k) {
+    
+    if(s.length < k)
+        return false;
+    
+    let map1 = new Map();
+
+    for(let i=0;i<s.length;i++)
+        map1.set(s[i], (map1.get(s[i]) || 0) +1);
+
+    let countWithOdd =0;
+    for(let value of map1.values()){
+        if(value %2 !== 0)
+            countWithOdd++;
+    }
+
+    if(countWithOdd > k)
+        return false;
+    
+    else
+        return true;
+};
