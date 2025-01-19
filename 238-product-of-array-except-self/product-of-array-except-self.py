60ff7c844eb78363a8d96deb5792b0eb3e0eae13
RanjanaRaghavan <52031047+RanjanaRaghavan@users.noreply.github.com>
2025-01-19 13:07:45 -0800

    Time: 31 ms (35.60%) | Memory: 26.9 MB (12.86%) - LeetSync

---
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -0,0 +1,30 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+
+        '''
+        3 lists
+        one for left and right each
+        one for ans
+        '''
+        left_arr = [1] * len(nums)
+        right_arr = [1] * len(nums)
+        ans = [1] * len(nums)
+
+
+        #for loop to iterate through every element of array left to right
+        for i in range(1,len(nums)):
+
+            #left array will have everything i-1
+            left_arr[i] = left_arr[i-1] * nums[i-1]
+
+        # for loop to iterate from right to left also left * right
+        for i in range(len(nums)-2 ,-1,-1):
+            #right will have everything i+1
+            right_arr[i] = right_arr[i+1] * nums[i+1]
+
+        #return 
+        for i in range(len(nums)):
+            ans[i] = right_arr[i] * left_arr[i]
+            
+        return ans
+        
