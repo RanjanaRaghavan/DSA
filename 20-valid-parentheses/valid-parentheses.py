4501b451ac31396b5504a64047247cf1e086d2b8
RanjanaRaghavan <52031047+RanjanaRaghavan@users.noreply.github.com>
2025-01-19 12:43:51 -0800

    Time: 0 ms (100.00%) | Memory: 17.8 MB (29.38%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,30 +1,49 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        
+
+        #Edge Cases
+        if len(s) == 0:
+            return True
         if len(s) %2 != 0:
             return False
         
-        #Stack to store the open parantheses
-        paranStack = []
+        '''
+        # Define a Dict of kV pairs of parantheses
+         Key :opening bracket
+         value :closing bracket
+        '''
+        paran_dict = {
+            '(' : ')',
+            '{' : '}',
+            '[' : ']'
+            }
 
+        #Define a stack
+        paran_stack = []
+
+        #Loop through s
         for i in range(len(s)):
-            if s[i] == '(' or s[i] == '[' or s[i]=='{':
-                paranStack.append(s[i])
+
+            # if opening bracket of any kind i will add to stack
+            if s[i] == '(' or s[i] == '{' or s[i] == '[':
+                paran_stack.append(s[i])
+
+            #else then check if top Element in stack is value of the key 
+            #if its is true pop the stack
+            #else return false
             else:
-                if(len(paranStack) > 0):
-                    topEle = paranStack[len(paranStack)-1]
+                if len(paran_stack) > 0 and s[i] == paran_dict[paran_stack[-1]]:
+                    paran_stack.pop()
                 else:
                     return False
-    
-                if (s[i] == ')' and topEle == '(') or (s[i] == ']' and topEle == '[') or (s[i] == '}' and topEle == '{'):
-                    paranStack.pop()
-                else :
-                    return False
+
+    #check if len(stack) is 0
+    #if 0 return true
+    #else Fasle
+
         
-        if len(paranStack) == 0:
-            return True
-        else:
-            return False
+        return len(paran_stack) == 0
+    
 
 
         
