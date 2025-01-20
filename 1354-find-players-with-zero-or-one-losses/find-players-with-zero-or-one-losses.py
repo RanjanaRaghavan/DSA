ce6b18c973f4dd322a591224314ab34bda5b3191
RanjanaRaghavan <52031047+RanjanaRaghavan@users.noreply.github.com>
2025-01-20 09:46:09 -0800

    Time: 148 ms (32.02%) | Memory: 63.3 MB (24.29%) - LeetSync

--- 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.py
+++ 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.py
@@ -24,27 +24,12 @@ class Solution:
         winner_dict = collections.Counter(match[0] for match in matches)
         loser_dict =  collections.Counter(match[1] for match in matches)
 
-        #print(winner_dict,loser_dict)
-
-        #list of all players that have not lost any matches.
-        ''' 
-            1.Winner List
-                We need to compare both lists and ensure the loser list doesnt 
-                have key which is in winner list
-        '''
+        loser_copy = loser_dict.copy()
         
-        for key in loser_dict.keys():
+        for key,value in loser_copy.items():
 
             if key in winner_dict:
                 winner_dict.pop(key)
-
-        '''
-            2.Loser List
-                a list of all players that have lost exactly one match.
-                so value must be 1
-        '''
-        loser_copy = loser_dict.copy()
-        for key,value in loser_copy.items():
             
             if value != 1:
                 loser_dict.pop(key)
