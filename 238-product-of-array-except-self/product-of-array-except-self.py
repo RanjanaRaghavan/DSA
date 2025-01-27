45b45bc62e34d693f2692efc0d9bed6b0045e48c
RanjanaRaghavan <52031047+RanjanaRaghavan@users.noreply.github.com>
2025-01-27 14:39:40 -0800

    Time: 51 ms (8.22%) | Memory: 27.4 MB (9.67%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,33 +1,20 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
+        
+        #base values
+        prefix_arr = [1] * len(nums)
+        suffix_arr = [1] * len(nums)
 
-        '''
-        3 lists
-        one for left and right each
-        one for ans
-        '''
-        left_arr = [1] * len(nums)
-        right_arr = [1] * len(nums)
         ans = [1] * len(nums)
 
+        for i in range(1,len(prefix_arr)):
+            prefix_arr[i] = prefix_arr[i-1] * nums[i-1]
 
-        #for loop to iterate through every element of array left to right
-        for i in range(1,len(nums)):
-
-            #left array will have everything i-1
-            left_arr[i] = left_arr[i-1] * nums[i-1]
-
-        # for loop to iterate from right to left also left * right
-        for i in range(len(nums)-1 ,-1,-1):
-            if i == len(nums)-1:
-                ans[i] = left_arr[i]
-                continue
-            
-            #right will have everything i+1
-            right_arr[i] = right_arr[i+1] * nums[i+1]
-
-            ans[i] = right_arr[i+1] * nums[i+1] * left_arr[i]
-
-            
-        return ans
+        for i in range(len(nums)-2,-1,-1):
+            suffix_arr[i] = suffix_arr[i+1] * nums[i+1]
         
+        print(prefix_arr,suffix_arr)
+        for i in range(len(nums)):
+            ans[i] = prefix_arr[i] * suffix_arr[i]
+        
+        return ans
