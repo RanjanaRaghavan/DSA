c98853ae0c03a27d638bedafeaabbfc47e366df6
RanjanaRaghavan <52031047+RanjanaRaghavan@users.noreply.github.com>
2025-02-05 13:31:21 -0800

    Time: 0 ms (100.00%) | Memory: 18.1 MB (7.27%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -2,30 +2,26 @@ class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
 
         '''
-        prefix = flower
-        5
-        0
-
-
+            1. keep thge first word as prefix
+            2. Then loop thru every word
+                1. compare every character and update prefix
+            3. return Prefix
         '''
-        if len(strs) == 0:
-            return ""
-        if len(strs) == 1:
-            return strs[0]
 
         prefix = strs[0]
 
-        for word in strs[1:]:
+        for s in strs:
 
-            #Cut the prefix short if next word is shorter
-            if len(word) < len(prefix):
-                prefix = prefix[:len(word)]
-
-            pref_len = len(prefix)
-            for i in range(pref_len):
-                if word[i] != prefix[i]:
-                    pref_len = i
-                    break
-
-            prefix = prefix[:pref_len]
+            while not s.startswith(prefix):
+                prefix =prefix[:-1] 
+            
         return prefix
+
+
+
+
+
+
+
+
+        
