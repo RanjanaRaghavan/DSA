11602c9310c00308b6df968ca3535bcdacd81e81
RanjanaRaghavan <52031047+RanjanaRaghavan@users.noreply.github.com>
2025-02-06 11:27:28 -0800

    Time: 18 ms (80.60%) | Memory: 30.7 MB (39.58%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.py
+++ 219-contains-duplicate-ii/contains-duplicate-ii.py
@@ -4,15 +4,13 @@ class Solution:
         map1 = collections.defaultdict()
 
         for i,n in enumerate(nums):
-            if n in map1 and abs(map1[n] -i) <=k:
+
+            if n in map1 and abs(map1[n] - i) <= k:
                 return True
             else:
                 map1[n] = i
-        
+
         return False
-        
 
-        
 
-            
         
