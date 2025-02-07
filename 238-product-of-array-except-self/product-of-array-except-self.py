21235f220b882c61e60e3e2059e89a3ce98ae09f
RanjanaRaghavan <52031047+RanjanaRaghavan@users.noreply.github.com>
2025-02-07 11:24:58 -0800

    Time: 43 ms (37.38%) | Memory: 21.7 MB (36.33%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,19 +1,32 @@
-class Solution:
-    def productExceptSelf(self, nums: List[int]) -> List[int]:
-        
-        #base values
-        prefix_arr = [1] * len(nums)
-        suffix_arr = [1] * len(nums)
+class Solution(object):
+    def productExceptSelf(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        '''
+        1. Calculate left product
+        2. Calculate right product
+        3. Multiply both
+        '''
+        left = [1]
+        right = [1]
+        ans = [0] * len(left)
 
-        ans = [1] * len(nums)
+        #left Prefix Profuct
 
-        for i in range(1,len(prefix_arr)):
-            prefix_arr[i] = prefix_arr[i-1] * nums[i-1]
+        for i in range(0,len(nums)-1):
+            left.append(left[-1] * nums[i])
 
-        for i in range(len(nums)-2,-1,-1):
-            suffix_arr[i] = suffix_arr[i+1] * nums[i+1]
+        #Right Prefix Product
+        for i in range(len(nums)-1,0,-1):
+            right.append(right[-1] * nums[i])
+
+        right = right[::-1]
+        print(len(right),len(left))
+        #Ans
+        for i in range(len(left)):
+            right[i] = left[i] * right[i]
         
-        for i in range(len(nums)):
-            ans[i] = prefix_arr[i] * suffix_arr[i]
+        return right
         
-        return ans
