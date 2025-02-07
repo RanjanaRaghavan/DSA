b016908a4b4f4b315499896dc08983ea4e1c97d5
RanjanaRaghavan <52031047+RanjanaRaghavan@users.noreply.github.com>
2025-02-07 11:30:01 -0800

    Time: 15 ms (96.00%) | Memory: 16.1 MB (64.95%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,8 +1,15 @@
-class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+
         anagrams = collections.defaultdict(list)
+
         for word in strs:
             key = ''.join(sorted(word))
             anagrams[key].append(word)
         
         return [v for v in anagrams.values()]
+        
