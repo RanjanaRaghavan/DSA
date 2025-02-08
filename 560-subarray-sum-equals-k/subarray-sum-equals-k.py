5221388936a97a658dbf36b29ceabce4d74f0a20
RanjanaRaghavan <52031047+RanjanaRaghavan@users.noreply.github.com>
2025-02-08 15:21:10 -0800

    Time: 41 ms (25.31%) | Memory: 20.4 MB (33.59%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,40 +1,25 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-
-        '''
-            Prefix Sum
-
-            1. Have a dict to store prefix_sum
-            2. pre set dict[0] as 1 
-            3. Have a result no_of_subarrays counter
-            4. loop through nums 
-                * if we see the comp in dict
-                    Add to result the ctr[comp]
-                * keep setting the prefix sums and freq in dict
-        '''
-
+        
         #Base Case
         if len(nums) == 0:
             return 0
-        
-        # if len(nums) ==1 and nums[0] == k:
-        #     return 1
-        
-
+            
         pref_dict = collections.defaultdict(int)
-        pref_dict[0] = 1
+        pref_dict[0] =1
 
-        result = 0
         pref_sum = 0
-
-        for num in nums:
-
-            pref_sum += num
-            comp = pref_sum - k
-
+        res = 0
+        for n in nums:
+            pref_sum +=n
+            comp = pref_sum - k 
+            
             if comp in pref_dict:
-                result += pref_dict[comp]
-
+                res += pref_dict[comp]
+            
             pref_dict[pref_sum] +=1
         
-        return result
+        return res
+
+
+        
