f5d34a2e325e263922a523fa6121d8fba969a2ac
RanjanaRaghavan <52031047+RanjanaRaghavan@users.noreply.github.com>
2025-02-10 12:58:32 -0800

    Time: 15 ms (82.45%) | Memory: 17.5 MB (95.83%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,27 +1,18 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
 
-        '''
-            1. Have a map to store chars (we dont want repeating chars)
-            2. two vars left and right to caluclate length
-            3. while u see a repeated char and the value of its last index is greater 
-            than left move left pointer to that place + 1 cuz we know thats wer our streak breaks
-            4. return max length
-        '''
-
         map1 = {}
-        max_count = 0
-        left = 0
+        left =0
+        c = 0
 
         for right in range(len(s)):
 
             if s[right] in map1 and map1[s[right]] >= left:
-                left = map1[s[right]] +1
-            
-                
+                left = map1[s[right]]+1
+
             map1[s[right]] = right
-            max_count = max(max_count, right - left +1)
+            c = max(c, right - left +1)
         
-        return max_count
+        return c
 
         
