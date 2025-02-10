9cc0a2a35ad6bea84775cc2e6c77b29a8fe722c1
RanjanaRaghavan <52031047+RanjanaRaghavan@users.noreply.github.com>
2025-02-10 14:06:33 -0800

    Time: 29 ms (79.96%) | Memory: 18.2 MB (45.74%) - LeetSync

--- 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.py
+++ 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.py
@@ -1,30 +1,25 @@
 class Solution:
     def canConstruct(self, s: str, k: int) -> bool:
 
-        #Edge Cases
+        #Base case
         if len(s) < k:
             return False
-        if len(s) == k:
-            return True
 
-        #Create an Array of 26 length filled with 0
-        lettersArray = [0] * 26
+        ctr = Counter(s)
+        odd = 0
 
-        #loop through the string s and count the freq of each char
-        for i in range(len(s)):
-            pos = ord(s[i]) - 97
-            lettersArray[pos] +=1
-        
-        #Calculate the number of chars with odd freq
-        oddCount =0
-        for i in range(26):
-            if lettersArray[i] %2 !=0:
-                oddCount +=1
+        for val,freq in ctr.items():
+            if freq %2 !=0:
+                odd +=1
 
-        #Logic
-        if oddCount >k:
+        if odd > k:
             return False
         else:
             return True
+
+
+
+
+        
 
         
