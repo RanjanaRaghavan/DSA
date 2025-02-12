d8132df9e8878124d4c129db38b6490e56a46726
RanjanaRaghavan <52031047+RanjanaRaghavan@users.noreply.github.com>
2025-02-12 14:54:14 -0800

    Time: 11 ms (36.78%) | Memory: 18.1 MB (67.98%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -2,9 +2,10 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
 
         ispal = ''
+
         for c in s:
+
             if c.isalnum():
                 ispal += c.lower()
-        
-        return ispal == ispal[::-1]
-        
+
+        return ispal == ispal[::-1]        
