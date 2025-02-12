46bd36166fe99dd7adb1a12f0c8320a6a283c827
RanjanaRaghavan <52031047+RanjanaRaghavan@users.noreply.github.com>
2025-02-12 11:29:36 -0800

    Time: 0 ms (100.00%) | Memory: 17.6 MB (95.41%) - LeetSync

--- 777-toeplitz-matrix/toeplitz-matrix.py
+++ 777-toeplitz-matrix/toeplitz-matrix.py
@@ -1,10 +1,18 @@
 class Solution:
     def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
 
-        for i in range(len(matrix)-1):
-            for j in range(len(matrix[0]) -1):
-                if matrix[i][j] != matrix[i+1][j+1]:
+        map1 = {}
+
+        for r in range(len(matrix)):
+            for c in range(len(matrix[0])):
+
+                if r-c not in map1:
+                    map1[r-c] = matrix[r][c]
+                elif map1[r-c] != matrix[r][c]:
                     return False
         
         return True
+
+
+
         
