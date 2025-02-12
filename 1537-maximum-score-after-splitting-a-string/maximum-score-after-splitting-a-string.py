6c5b70a81f190304757b17819106e8ecf6b66ccc
RanjanaRaghavan <52031047+RanjanaRaghavan@users.noreply.github.com>
2025-02-12 15:41:36 -0800

    Time: 3 ms (49.10%) | Memory: 17.8 MB (61.48%) - LeetSync

--- 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.py
+++ 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.py
@@ -1,33 +1,27 @@
 class Solution:
     def maxScore(self, s: str) -> int:
 
-        slist = list(map(int, s))
-        
+        slist = list(map(int,s))
+
         total = sum(slist)
 
-        #initial Calc
+        #initial check
         if slist[0] == 0:
             left = 1
             right = total
         else:
-            left =0
+            left = 0
             right = total -1
         
         score = left + right
         
-        for i in range(1,len(slist)-1):
-
+        for i in range(1, len(s)-1):
             if slist[i] == 0:
-                left += 1
+                left +=1
             else:
-                right -= 1
-
+                right -=1
+            
             score = max(score, left+right)
-        
+            
         return score
-
-
-
-
-        
         
