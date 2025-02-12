e82823a994accf4ed3d71807cbf76e010db643d9
RanjanaRaghavan <52031047+RanjanaRaghavan@users.noreply.github.com>
2025-02-12 14:53:17 -0800

    Time: 0 ms (100.00%) | Memory: 17.8 MB (77.33%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,23 +1,19 @@
 class Solution:
     def isValid(self, s: str) -> bool:
 
-        #Edge Case
-        if len(s) == 0:
-            return False
-
-        if len(s) %2 == 1:
-            return False
-
         map1 = {
-            '(' :')',
-            '{' :'}',
+            '(' : ')',
+            '{' : '}',
             '[' : ']'
         }
+
         stack1 = []
 
         for c in s:
+
             if c in map1:
                 stack1.append(c)
+            
             else:
                 if stack1 and map1[stack1[-1]] == c:
                     stack1.pop()
@@ -25,5 +21,4 @@ class Solution:
                     return False
         
         return len(stack1) == 0
-
-
+        
