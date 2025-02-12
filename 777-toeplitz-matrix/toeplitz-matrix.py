d8e053852e03f46308c989a9c944e144d513b773
RanjanaRaghavan <52031047+RanjanaRaghavan@users.noreply.github.com>
2025-02-12 14:39:14 -0800

    Time: 3 ms (39.03%) | Memory: 18 MB (28.53%) - LeetSync

--- 777-toeplitz-matrix/toeplitz-matrix.py
+++ 777-toeplitz-matrix/toeplitz-matrix.py
@@ -5,9 +5,11 @@ class Solution:
 
         for r in range(len(matrix)):
             for c in range(len(matrix[0])):
+
                 if r-c not in map1:
                     map1[r-c] = matrix[r][c]
                 elif map1[r-c] != matrix[r][c]:
                     return False
         
         return True
+         
