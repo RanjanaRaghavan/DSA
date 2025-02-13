6fe70a9ebdaf5f788213ec09078b5c1ffe24c68f
RanjanaRaghavan <52031047+RanjanaRaghavan@users.noreply.github.com>
2025-02-13 10:49:06 -0800

    Time: 0 ms (100.00%) | Memory: 18.9 MB (72.11%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -6,7 +6,7 @@ class Solution:
         for i in range(1,len(nums)):
             if nums[i] != nums[i-1]:
                 nums[k] = nums[i]
-                k+=1
+                k +=1
         
         return k
         
