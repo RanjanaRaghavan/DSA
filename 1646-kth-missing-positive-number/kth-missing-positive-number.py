60d7b46f0100379e320365e2c738efa4cce0ecdb
RanjanaRaghavan <52031047+RanjanaRaghavan@users.noreply.github.com>
2025-02-13 11:12:46 -0800

    Time: 0 ms (100.00%) | Memory: 17.7 MB (93.52%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.py
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.py
@@ -10,8 +10,9 @@ class Solution:
 
             if arr[mid] - mid - 1 < k:
                 left = mid +1
+            
             else:
                 right = mid -1
-        
+
         return left +k 
         
