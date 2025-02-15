9dfaa7bec10b8dfc87a1276cc02c9dd9807f4cc8
RanjanaRaghavan <52031047+RanjanaRaghavan@users.noreply.github.com>
2025-02-15 12:59:56 -0800

    Time: 23 ms (18.17%) | Memory: 25.1 MB (86.88%) - LeetSync

--- 975-range-sum-of-bst/range-sum-of-bst.py
+++ 975-range-sum-of-bst/range-sum-of-bst.py
@@ -6,22 +6,22 @@
 #         self.right = right
 class Solution:
     def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
-
+        
         def dfs(node):
-            cur_sum = 0
-
+            sum = 0
             if not node:
                 return 0
             
             if low <= node.val <= high:
-                cur_sum += node.val
-            
+                sum += node.val
+
             left = dfs(node.left)
             right = dfs(node.right)
 
-            return cur_sum + left + right
+            return sum + left + right
         
         return dfs(root)
+    
 
 
         
