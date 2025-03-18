e205c7a820081e8b9af40c3f45a645ff33d854a9
RanjanaRaghavan <52031047+RanjanaRaghavan@users.noreply.github.com>
2025-03-17 20:24:15 -0700

    Time: 1 ms (45.20%) | Memory: 19.1 MB (29.81%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
@@ -7,46 +7,30 @@
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
 
-        '''
-        1. maxcount to keep track of maxCount
-        2. current count 
-        3. Helper Function call
-        4. return maxcount 
-        '''
-
-        max_count = [0]
+        maxCount = [0]
         cur_count = 0
-        self.dfs(root,cur_count,max_count)
-        
-        return max_count[0]
+        self.dfs(root,maxCount,cur_count)
+
+        return maxCount[0]
+
+
     
-    '''
-    Pre order traversal
-    Helper Function
-    Input
-    1. root
-    2.cur count
-    3 max count
+    def dfs(self,node,maxCount,cur_Count):
 
-    Do: Calculate cur count
-        Calculate MaxCount
-    '''
-    def dfs(self,root,cur_count,max_count):
-
-        #None check
-        if root is None:
+        if node is None:
             return None
-
-        #increase count
-        cur_count += 1
-
-        #Leaf node check (check for maxCount)
-        if root.left is None and root.right is None:
-            max_count[0] = max(max_count[0],cur_count)
-
-        #Non leaf node 
-        if root.left:
-            self.dfs(root.left,cur_count,max_count)
         
-        if root.right:
-            self.dfs(root.right,cur_count,max_count)
+        cur_Count +=1
+
+        #Leaf Node
+        if node.right is None and node.left is None:
+            maxCount[0] = max(maxCount[0], cur_Count)
+        
+        #Non Leaf
+        if node.left:
+            self.dfs(node.left,maxCount,cur_Count)
+        
+        if node.right:
+            self.dfs(node.right,maxCount,cur_Count)
+
+        
