25965f6b5cad2696d285a66d4cc61052971de905
RanjanaRaghavan <52031047+RanjanaRaghavan@users.noreply.github.com>
2025-05-01 15:37:50 -0700

    Time: 29 ms (37.70%) | Memory: 27.5 MB (5.53%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,32 +1,24 @@
-class Solution(object):
-    def productExceptSelf(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
-        '''
-        1. Calculate left product
-        2. Calculate right product
-        3. Multiply both
-        '''
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+
         left = [1]
         right = [1]
-        ans = [0] * len(left)
 
-        #left Prefix Profuct
+        ans = [0] * len(nums)
 
+        #calculate left sum
         for i in range(0,len(nums)-1):
             left.append(left[-1] * nums[i])
 
-        #Right Prefix Product
-        for i in range(len(nums)-1,0,-1):
+        #calculate right sum
+        for i in range(len(nums)-1, 0, -1):
             right.append(right[-1] * nums[i])
 
         right = right[::-1]
-        print(len(right),len(left))
-        #Ans
-        for i in range(len(left)):
-            right[i] = left[i] * right[i]
-        
-        return right
+        #ans = prefix * suffix
+        for i in range(len(nums)):
+            ans[i] = left[i] * right[i]
+
+        return ans
+
         
