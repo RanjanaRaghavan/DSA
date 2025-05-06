14bac8f2551a2b533406b25d0754311e171a762d
RanjanaRaghavan <52031047+RanjanaRaghavan@users.noreply.github.com>
2025-05-05 20:55:09 -0700

    Time: 263 ms (38.87%) | Memory: 20.3 MB (58.24%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,44 +1,36 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
 
-        def bfs(grid,i,j,visited):
+        def bfs(grid,i ,j,visited):
 
             Q = collections.deque([[i,j]])
             visited[i][j] = 1
 
             while Q:
-
                 r,c = Q.popleft()
-                
-                
-                directions = [(0,1),(0,-1),(1,0),(-1,0)]
+
+                directions = [(1,0),(-1,0),(0,1),(0,-1)]
 
                 for dr,dc in directions:
+                    nr = dr +r
+                    nc = dc +c
 
-                    nr = r +dr
-                    nc = c +dc
-
-                    if 0 <= nr < len(grid) and 0<= nc < len(grid[0]) and grid[nr][nc] == "1" and visited[nr][nc] == 0:
-                        visited[nr][nc] =1
+                    if 0<= nr <len(grid) and 0<=nc < len(grid[0]) and grid[nr][nc] == "1" and visited[nr][nc] == 0:
+                        visited[nr][nc] = 1
                         Q.append([nr,nc])
 
 
-        visited = [[0] * len(grid[0]) for _ in range(len(grid))]
-        count = 0
-
-        if not grid :
+        if not grid:
             return 0
+        
+        visited = [[0] * len(grid[0]) for _ in range(len(grid))] 
+        count = 0
 
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j] == "1" and visited[i][j] == 0:
                     bfs(grid,i,j,visited)
                     count +=1
-    
+        
         return count
-
-
-
-        
-
-        
+         
