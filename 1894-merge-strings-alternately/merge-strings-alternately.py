0b788f5212bb99548eb9e54253b6ae5b66f5d15a
Ranjana Raghavan <52031047+RanjanaRaghavan@users.noreply.github.com>
2025-07-22 10:31:38 -0700

    Time: 38 ms (56.35%) | Memory: 17.9 MB (33.39%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.py
+++ 1894-merge-strings-alternately/merge-strings-alternately.py
@@ -7,14 +7,15 @@ class Solution:
         '''
         l1 = len(word1)
         l2 = len(word2)
-        ans = ''
+        ans = []
         for i in range(0,min(l1,l2)):
-                ans += word1[i] +word2[i]
+                ans.append(word1[i]) 
+                ans.append(word2[i])
         
         if l1 > l2:
-            ans += word1[l2:]
+            ans.append(word1[l2:])
         else:
-            ans += word2[l1:]
-        return ans
+            ans.append(word2[l1:])
+        return ''.join(ans)
 
         
