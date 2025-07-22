3011aecc48a5fb2c921178e624893052e248a939
Ranjana Raghavan <52031047+RanjanaRaghavan@users.noreply.github.com>
2025-07-22 11:57:09 -0700

    Time: 0 ms (100.00%) | Memory: 17.9 MB (26.88%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
@@ -1,10 +1,40 @@
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
 
-        if  str1+str2 != str2+str1:
-            return ""
+        '''
+        Approach
+        1. Check if both strings have the same letters repeating if not return empty
+        2. Find GCD of the lengths and return
+        '''
+        '''
+        Time Complexity : O(m+n) -> m = len(str1) n = len(str2)
+        Space Complexity : O(m+n) -> m = len(str1) n = len(str2)
+        '''
+
+        if str1 + str2 != str2 +str1:
+            return ''
         
-        gcf = gcd(len(str1), len(str2))
-        return str1[:gcf]
-        
+        gcf = gcd(len(str1) , len(str2))
+
+        return str1[0:gcf]
+
+# def test_gcdOfStrings():
+
+#     sol = Solution()
+
+#     #min
+#     assert sol.gcdOfStrings("A","A") == "A"
+#     #max
+#     assert sol.gcdOfStrings("ABC","ABCABC") == "ABC"
+#     #same
+#     assert sol.gcdOfStrings("ABC","ABC") == "ABC"
+#     #regular
+#     assert sol.gcdOfStrings("ABCABC","ABC") == "ABC"
+#     #no op
+#     assert sol.gcdOfStrings("LEET","CODE") == ""
+
+# test_gcdOfStrings()
+# print("All Tests passed")
+
+
         
