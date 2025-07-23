02e9221c87e8c1be801b5f1d96a202c4d3f0ecb3
Ranjana Raghavan <52031047+RanjanaRaghavan@users.noreply.github.com>
2025-07-23 14:06:38 -0700

    Time: 47 ms (9.58%) | Memory: 27.6 MB (5.19%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,24 +1,43 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
+        
+        n = len(nums)
+        answer = [1] * n
 
-        left = [1]
-        right = [1]
+        pref_prod = [1] * n
+        suff_prod = [1] * n
 
-        ans = [0] * len(nums)
+        for i in range(1,n):
+            pref_prod[i] = pref_prod[i-1] * nums[i-1]
+        print("pref" , pref_prod)
+        
+        for i in range(n-2,-1,-1):
+            suff_prod[i] = suff_prod[i+1] * nums[i+1]
+        print("suff" , suff_prod)
 
-        #calculate left sum
-        for i in range(0,len(nums)-1):
-            left.append(left[-1] * nums[i])
+        for i in range(n):
+            answer[i] = pref_prod[i] * suff_prod[i]
 
-        #calculate right sum
-        for i in range(len(nums)-1, 0, -1):
-            right.append(right[-1] * nums[i])
+        
+        return answer
 
-        right = right[::-1]
-        #ans = prefix * suffix
-        for i in range(len(nums)):
-            ans[i] = left[i] * right[i]
+# def test_productExceptSelf():
 
-        return ans
+#     sol = Solution()
+
+#     assert sol.productExceptSelf([1,2,3,4]) == [24,12,8,6]
+#     #min
+#     assert sol.productExceptSelf([-1,1,0,-3,3]) == [0,0,9,0,0]
+#     #max
+#     assert sol.productExceptSelf([1,2,3,4]) == [24,12,8,6]
+#     #same
+#     assert sol.productExceptSelf([1,1,1,1]) == [1,1,1,1]
+#     #no op
+#     assert sol.productExceptSelf([0,0]) == [0,0]
+
+# test_productExceptSelf()
+# print("All tests passed")
+
+
 
         
