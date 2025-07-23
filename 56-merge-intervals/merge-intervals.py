0aaf33508901077f54c7aae5c96f01b3c8710414
Ranjana Raghavan <52031047+RanjanaRaghavan@users.noreply.github.com>
2025-07-23 11:51:43 -0700

    Time: 10 ms (34.79%) | Memory: 21.1 MB (42.46%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -1,16 +1,43 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
 
+        '''
+        Approach
+        1. Sort list and add first element to new list
+        2. if newlist[-1][1] < intervals[i][0]
+        3. Then min (newlist[-1][0] , intervals[i][0])
+            a. max (newlist[-1][1] < intervals[i][1])
+            b. else add the interval in
+        4. return new list
+        '''
+        '''
+        Time Complexity : O(n) -> n = len(intervals)
+        Space Complexity : O(n) -> n = len(intervals) (worst case scenario of no overlap)
+        '''
         intervals.sort()
-        res = [intervals[0]]
+        merged_intervals = [intervals[0]]
 
-        for i in range( 1, len(intervals)):
-
-            if res[-1][1] >= intervals[i][0]:
-                res[-1][0] = min(intervals[i][0] , res[-1][0])
-                res[-1][1] = max(intervals[i][1], res[-1][1])
+        for i in range(1,len(intervals)):
+            if merged_intervals[-1][1] >= intervals[i][0]:
+                merged_intervals[-1][0] = min(merged_intervals[-1][0] , intervals[i][0])
+                merged_intervals[-1][1] = max(merged_intervals[-1][1], intervals[i][1])
             else:
-                res.append(intervals[i])
-            
-        return res
+                merged_intervals.append(intervals[i])
+                
+        
+        return merged_intervals
+
+def test_merge():
+
+    sol = Solution()
+
+    assert sol.merge([[1,3],[2,6],[8,10],[15,18]]) == [[1,6],[8,10],[15,18]]
+    assert sol.merge([[1,3],[2,6],[8,10],[15,18]]) == [[1,6],[8,10],[15,18]]
+    assert sol.merge([[1,3],[2,6],[8,10],[15,18]]) == [[1,6],[8,10],[15,18]]
+    
+
+        
+
+
+
         
